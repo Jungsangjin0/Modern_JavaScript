04f69eb4f11cd1a784a6e678aad66205c19f882b
sangjin <loky13@daum.net>
2021-02-20 03:16:54 +0900

    day3 2021-02-20

---
+++ jsFolder/day3.js
@@ -0,0 +1,18 @@
+//문자형
+let value = true;
+alert(typeof value) //boolean
+
+value = String(value)
+alert(typeof value) // string
+
+
+//숫자형
+alert("6" / "2") //3
+let str = "123";
+alert(typeof str) //string
+let num = Number(str)
+alert(typeof num) //number
+
+let age = Number("임의의 문자열 123")
+alert(age) //NaN
+
